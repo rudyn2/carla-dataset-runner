572f7b90c4d2489d3a6d64b9e1de7b0ed6d427be
rudyn2 <c01010010@hotmail.com>
2021-04-12 11:44:47 -0400

    add .dockerignore

--- scripts/run_collector.sh
+++ scripts/run_collector.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvidia-docker run -v ~/cachefs/:/home/carla-dataset-runner/data --network host carla-dataset-collector -e DOCKER_HOST_IP=172.18.0.1 /bin/bash
+nvidia-docker run -v ~/cachefs/:/home/carla-dataset-runner/data -e DOCKER_HOST_IP="172.18.0.1" --network host carla-dataset-collector /bin/bash
